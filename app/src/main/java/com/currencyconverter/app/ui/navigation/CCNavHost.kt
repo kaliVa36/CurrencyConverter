8bd1b32ee26a6f28a1b8a38f0970178767f437a2
Kalina Valeva <>
2025-04-27 21:56:59 +0300

    - navigation extension with params - passed data to second screen

--- app/src/main/java/com/currencyconverter/app/ui/navigation/CCNavHost.kt
+++ app/src/main/java/com/currencyconverter/app/ui/navigation/CCNavHost.kt
@@ -1,11 +1,13 @@
 package com.currencyconverter.app.ui.navigation
 
+import android.util.Log
 import androidx.compose.animation.EnterTransition
 import androidx.compose.animation.ExitTransition
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
+import androidx.core.os.bundleOf
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -18,26 +20,40 @@ import com.currencyconverter.app.ui.feature.SelectionAndInputViewModel
 fun CCNavHost(navController: NavHostController) {
     NavHost(
         navController = navController,
-        startDestination = Route.FIRST_SCREEN,
+        startDestination = NavigationConstants.FIRST_SCREEN,
         enterTransition = { EnterTransition.None },
         exitTransition = { ExitTransition.None },
         popEnterTransition = { EnterTransition.None },
         popExitTransition = { ExitTransition.None }
     ) {
-        composable(Route.FIRST_SCREEN) {
+        composable(NavigationConstants.FIRST_SCREEN) {
             val viewModel = hiltViewModel<SelectionAndInputViewModel>()
             val data by viewModel.selectionAndInputDataState.collectAsState()
             SelectionAndInputScreen(
                 data = data,
                 onValueChanged = viewModel::onAmountValueChanged,
-                onContinueClicked = { navController.navigate(Route.SECOND_SCREEN) },
+                onContinueClicked = {
+                    navController.navigate(NavigationConstants.SECOND_SCREEN)
+                    navController.navigate(
+                        NavigationConstants.SECOND_SCREEN, bundleOf(
+                            Params.CURRENCY_FROM to data.selectedFromCurrency,
+                            Params.CURRENCY_TO to data.selectedToCurrency,
+                            Params.AMOUNT to data.amount
+                        )
+                    )
+                },
                 onItemFromCurrencySelected = viewModel::onDropdownItemFromCurrencySelected,
                 onItemToCurrencySelected = viewModel::onDropdownItemToCurrencySelected,
             )
         }
 
-        composable(Route.SECOND_SCREEN) {
-            DummyScreen(Color.Blue) { navController.navigate(Route.FIRST_SCREEN) }
+        composable(NavigationConstants.SECOND_SCREEN) { backStack ->
+            // Will be fixed soon
+            val currencyFrom = backStack.arguments?.getString(Params.CURRENCY_FROM)
+            val currencyTo = backStack.arguments?.getString(Params.CURRENCY_TO)
+            val amount = backStack.arguments?.getString(Params.AMOUNT)
+
+            DummyScreen(Color.Blue) { navController.navigate(NavigationConstants.FIRST_SCREEN) }
         }
     }
 }
--- app/src/main/java/com/currencyconverter/app/ui/navigation/Route.kt
+++
@@ -1,6 +0,0 @@
-package com.currencyconverter.app.ui.navigation
-
-object Route {
-    const val FIRST_SCREEN = "first_screen"
-    const val SECOND_SCREEN = "second_screen"
-}
